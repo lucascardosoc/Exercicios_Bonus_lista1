bb355571dc21065df8d3e3e4f5e53a6b45df6c29
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-08 07:33:47 -0300

    Exercício 5

--- src/Exercício005.java
+++ src/Exercício005.java
@@ -3,25 +3,31 @@ public class Exercício005 {
 
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner (System.in);
-		int trash;
+		int trash1, trash2, trash3;
 		
 		System.out.println("Por favor, digite o peso do 1º saco de lixo");
-		 trash = leitor.nextInt();
+		 trash1 = leitor.nextInt();
 		 
 		 System.out.println("Por favor, digite o peso do 2º saco de lixo");
-		 trash = leitor.nextInt();
+		 trash2 = leitor.nextInt();
 		 
 		 System.out.println("Por favor, digite o peso do 3º saco de lixo");
-		 trash = leitor.nextInt();
+		 trash3 = leitor.nextInt();
 		 
-		 if (trash<10) {
-			 System.out.println("O drone pode carregar o " + trash);
+		 if (trash1<trash2) {
+			 System.out.println("O drone pode carregar o 1º saco de lixo");
 		 }else {
-		 if (trash>10) 
-			 System.out.println("O drone não pode carregar nenhum dos lixos");
+			 if (trash2<trash3) {
+				 System.out.println("O drone pode carregar o 2º saco de lixo");
+			 }else {
+				 if (trash3<trash1) {
+					 System.out.println("O drone pode carregar o 3º saco de lixo");
+					 leitor.close();
+				 }
+			 }
 		 }
 		 
-		 leitor.close();
+		 
 
 	}
 
